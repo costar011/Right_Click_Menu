a01ddf89e5903c341492f172c6ced3d5bd88cdc7
costar011 <dnaer456@gmail.com>
2024-04-15 22:37:13 +0900

    js

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const links = document.querySelector(".links");
 
 container.addEventListener("contextmenu", handleClick);
 btn.addEventListener("click", handleClick);
+
+links.forEach((e)  => {
+ e.addEventListener("click", handleClick);   
+});
+
+function handleClick(e) {
+    const menu = document.getElementById("menu");
+}
