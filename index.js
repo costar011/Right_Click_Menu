d862cbccfcecd9359d60186b4e6426e90afd4593
costar011 <dnaer456@gmail.com>
2024-04-16 00:19:28 +0900

    js

--- index.js
+++ index.js
@@ -11,4 +11,17 @@ links.forEach((e)  => {
 
 function handleClick(e) {
     const menu = document.getElementById("menu");
+    const overlay = document.getElementById("overlay");
+
+    if(e.which == 3) {
+        e.preventDefault();
+
+        overlay.style.display = "block";
+        menu.style.display = "flex";
+        menu.style.opacity = "1";
+        menu.style.transform = `translate(${e.clientX}px, ${e.clientY}px) scale(1)`;
+    } else if (e.which === 1) {
+        overlay.style.display = "none";
+        menu.style.display = "none";
+    }
 }
